8527a4b03b72fb34ceb16291ccbfa68e37a81d76
noah <jeonghogim318@gmail.com>
2025-02-04 10:00:00 +0900

    Proxy Pattern

---
+++ src/_09_proxy/ex01/Image.java
@@ -0,0 +1,7 @@
+package _09_proxy.ex01;
+
+// Subject interface
+interface Image {
+    void display();
+    String getFileName();
+}
---
+++ src/_09_proxy/ex01/Main.java
@@ -0,0 +1,15 @@
+package _09_proxy.ex01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProxyImage image = new ProxyImage("test_image.jpg");
+
+        System.out.println("File name: " + image.getFileName());
+        System.out.println("File extension: " + image.getFileExtension());
+
+        image.display();
+        image.display();
+    }
+
+}
