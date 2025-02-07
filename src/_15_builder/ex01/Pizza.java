9a2aed717a3b81ded317d366de3fb2cba79e4614
noah <jeonghogim318@gmail.com>
2025-02-07 10:00:00 +0900

    Builder Pattern

---
+++ src/_15_builder/ex01/Pizza.java
@@ -0,0 +1,49 @@
+package _15_builder.ex01;
+
+public class Pizza {
+    private String dough;
+    private String sauce;
+    private String topping;
+
+    // Private constructor to enforce the use of Builder
+    private Pizza(PizzaBuilder builder) {
+        this.dough = builder.dough;
+        this.sauce = builder.sauce;
+        this.topping = builder.topping;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pizza with " + dough + " dough, "
+            + sauce + " sauce, and " + topping + " topping.";
+    }
+
+    public static class PizzaBuilder{
+        private String dough;
+        private String sauce;
+        private String topping;
+
+        public PizzaBuilder dough(String dough){
+            this.dough = dough;
+            return this;
+        }
+
+        public PizzaBuilder sauce(String sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+        public PizzaBuilder topping(String topping) {
+            this.topping = topping;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(this);
+        }
+
+    }
+
+
+
+}
