02bda15f6f01d12ab20ee9e53d7510d308343f7d
noah <jeonghogim318@gmail.com>
2025-02-09 10:00:00 +0900

    Prototype Pattern

---
+++ src/_19_prototype/ex01/Prototype.java
@@ -0,0 +1,5 @@
+package _19_prototype.ex01;
+
+interface Prototype {
+    Prototype clone();
+}
---
+++ src/_19_prototype/ex02/Prototype.java
@@ -0,0 +1,6 @@
+package _19_prototype.ex02;
+
+// Simple Prototype interface
+interface Prototype {
+    Prototype clone();
+}
