3c5f21ebaa67337d9ef16c3cb7cce519ff3e072c
noah <jeonghogim318@gmail.com>
2025-02-12 10:00:00 +0900

    Specification Pattern

---
+++ src/_25_specification/ex01/EvenSpecification.java
@@ -0,0 +1,8 @@
+package _25_specification.ex01;
+
+class EvenSpecification implements Specification {
+    @Override
+    public boolean isSatisfiedBy(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ src/_25_specification/ex01/Specification.java
@@ -0,0 +1,10 @@
+package _25_specification.ex01;
+
+public interface Specification {
+    boolean isSatisfiedBy(int number);
+
+    default Specification and(Specification other) {
+        return number -> this.isSatisfiedBy(number)
+               && other.isSatisfiedBy(number);
+    }
+}
